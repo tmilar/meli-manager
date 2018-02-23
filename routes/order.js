d4c9eb01f867999df1052e9bba414556a5e9fcd7
Tomas Milar <milar@strands.com>
2018-02-23 05:35:36 -0300

    Implement full /order endpoint:
    	* Fetch orders from Mercadolibre, with start+end+account filters.
    	* Ability to store orders to Spreadsheet.

--- routes/order.js
+++ routes/order.js
@@ -1,11 +1,43 @@
-var express = require('express');
-import moment from "moment";
-var router = express.Router();
+const router = require('express').Router();
+const moment = require('moment');
+const Account = require('../model/account');
+const OrderService = require("../service/orders.service.js");
+const Promise = require("bluebird");
 
-router.get('/order/fetch', (req, res) => {
-    let startStr = req.query.start;
-    let endStr = req.query.end;
-    let dateStart = moment(startStr, 'MM-DD-YY');
-    let dateEnd = moment(endStr, 'MM-DD-YY');
+/**
+ * @param start - start date range, format 'MM-DD-YY'
+ * @param end - end date range, format 'MM-DD-YY', default today.
+ * @param accounts - pick specific account usernames instead of all.
+ *
+ */
+router.get('/', async (req, res, next) => {
+    let {start, end, accounts, store} = req.query;
 
-})
+    // parse params
+    let dateStart = start && moment(start, 'DD-MM-YY').toDate();
+    let dateEnd = end && moment(end, 'DD-MM-YY').endOf('day').toDate();
+    let nicknamesFilter = accounts ? {nickname: {$in: JSON.parse(accounts)}}: {};
+    let shouldStore = store === "true" || store === "1";
+
+    //// *** orders service ***
+    // 1. get accounts from DB
+    let selectedAccounts = await Account.find(nicknamesFilter).lean().exec();
+
+    // 2. fetch remote orders for accounts
+    let orders = await OrderService.fetchMeliOrders(dateStart, dateEnd, selectedAccounts);
+
+    // 3. store selected orders
+    if(shouldStore) {
+        try {
+            await Promise.mapSeries(orders, o => OrderService.saveNewOrder(o));
+        } catch(e) {
+            debugger;
+            throw new Error(e);
+            // next(e);
+        }
+    }
+
+    res.json(orders);
+});
+
+module.exports = router;
--- service/orders.service.js
+++ service/orders.service.js
@@ -1,13 +1,40 @@
 const SheetsHelper = require("../lib/sheetsHelper");
 const Order = require("../model/order.js");
+const req = require('request-promise');
+const config = require('../config');
 
 class OrdersService {
-
+/*
     constructor({sheet}) {
         this.ordersSheet = sheet;
     }
+*/
+    static async setup() {
+        if(this.ready) return;
 
-    async saveNewOrder(newOrderBody) {
+        const ordersSpreadsheet = config.spreadsheet.orders;
+
+        this.ordersSheet = await SheetsHelper.setupSheet({
+            credentials: config.secrets.spreadsheet,
+            spreadsheetsKey: ordersSpreadsheet.id,
+            sheetName: ordersSpreadsheet.sheet
+        });
+
+        this.ready = true;
+    }
+
+    static async saveNewOrder(newOrderBody) {
+        /* // TODO make the sheets helper *be* the actual empowered sheet...
+         const ordersSheet = await SheetsHelper.setupSheet({
+         credentials: config.secrets.spreadsheet,
+         spreadsheetsKey: ordersSpreadsheet.id,
+         sheetName: ordersSpreadsheet.sheet
+         });
+         */
+
+        // TODO don't do the setup here... move elsewhere...
+        await this.setup();
+
         let newOrder = Order.buildFromMeliOrder(newOrderBody);
         let orderRow = newOrder.toRowArray();
         let newRowPosition = await SheetsHelper.getNextEmptyRowPosition(this.ordersSheet);
@@ -20,11 +47,35 @@ class OrdersService {
      *
      * @param startDate
      * @param endDate
+     * @param accounts
      * @returns {Promise.<void>}
      */
-    async fetchOrdersBetweenDates(startDate, endDate=new Date()) {
-        let url = "";
+    static async fetchMeliOrders(startDate, endDate, accounts) {
+        let apiUrl = "https://api.mercadolibre.com/orders/search";
 
+        // Build the request URLs for selected accounts
+        let orderRequests = accounts
+            .map(acc => apiUrl + `?seller=${acc.id}&access_token=${acc.auth.accessToken}&sort=date_desc`)
+            .map(orderRequest => req({uri: orderRequest, json: true}));
+
+        // Get request responses
+        let ordersResponses = await Promise.all(orderRequests);
+
+        // Flatten responses to one array
+        let orders = ordersResponses
+            .map(ordersResponse => ordersResponse.results)
+            .reduce((arr=[], order) => arr.concat(order) );
+
+        // Filter orders between startDate & endDate
+        let filteredOrders = orders.filter(order =>
+            (!startDate || (new Date(order["date_closed"]) >= startDate)) &&
+            (!endDate || (new Date(order["date_closed"]) <= endDate))
+        );
+
+        // sort orders by "date_closed"
+        let filteredOrdersSorted = filteredOrders.sort((a,b) => new Date(a["date_closed"]) - new Date(b["date_closed"]));
+
+        return filteredOrdersSorted;
     }
 }
 
