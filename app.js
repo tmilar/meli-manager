c049af719ec82fc1d6c2b2632aeb57aa6a64bb94
Tomas Milar <tomasmilar4@gmail.com>
2018-02-23 05:35:36 -0300

    Implement full /order endpoint:
    	* Fetch orders from Mercadolibre, with start+end+account filters.
    	* Ability to store orders to Spreadsheet.

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ require("./config/db");
 
 const index = require('./routes/index');
 const auth = require('./routes/auth');
+const order = require('./routes/order');
 
 const app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //setup routes
 app.use('/', index);
 app.use('/auth', auth);
+app.use('/order', order);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
