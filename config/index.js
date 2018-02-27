e1cd0a607d433e07b0941996cf7611375e608611
Tomas Milar <milar@strands.com>
2018-02-27 13:38:15 -0300

    Add 'Orders' + 'Customers' spreadsheets id and name to .env variables.

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ const config = {
         (process.env.MONGODB_URL || 'mongodb://localhost/melimgr'),
     spreadsheet: {
         orders: {
-            id: "1CPwBbJaDwLbv80S5__LK8_5LZOQ5NE5_zXo45um1LNQ",
-            sheet: "Ventas 2018",
+            id: process.env.ORDERS_SPREADSHEET_ID || "1CPwBbJaDwLbv80S5__LK8_5LZOQ5NE5_zXo45um1LNQ",
+            sheet: process.env.ORDERS_SPREADSHEET_SHEETNAME || "Ventas 2018",
             sheetId: 1
         },
         customers: {
-            id: "1CPwBbJaDwLbv80S5__LK8_5LZOQ5NE5_zXo45um1LNQ",
-            sheet: "Clientes"
+            id: process.env.CUSTOMERS_SPREADSHEET_ID || "1CPwBbJaDwLbv80S5__LK8_5LZOQ5NE5_zXo45um1LNQ",
+            sheet: process.env.CUSTOMERS_SPREADSHEET_SHEETNAME || "Clientes"
         }
     },
     auth: {
