20bf01a7099bb8f2c26172f4224f81a46e90f9db
Tomas Milar <milar@strands.com>
2018-03-11 15:13:25 -0300

    Remove unused 'next' variable from express router handlers.

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
 })
 
 // Error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   // Set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
