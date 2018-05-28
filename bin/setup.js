a19daf79c11bfd92064be226951d0c46b1335b14
Tomas Milar <tomasmilar4@gmail.com>
2018-05-28 16:20:49 -0300

    Add 'npm run setup -- --force' override option for a new fresh setup.

--- bin/setup.js
+++ bin/setup.js
@@ -1,9 +1,22 @@
 /**
  * Initialize .env file
+ * @param --force [optional] overrides the .env file even if it exists.
  */
-
 'use strict'
 const fs = require('fs')
 
-fs.createReadStream('.env.sample')
-  .pipe(fs.createWriteStream('.env'))
+const args = process.argv.slice(2)
+const force = args[0] === '--force'
+
+const ENV = '.env'
+const ENV_SAMPLE = '.env.sample'
+
+if (fs.existsSync(ENV) && !force) {
+  console.log(`${ENV} file already exists!`)
+  return
+}
+
+fs.createReadStream(ENV_SAMPLE)
+  .pipe(fs.createWriteStream(ENV))
+
+console.log(`Created ${ENV} file from ${ENV_SAMPLE}`)
