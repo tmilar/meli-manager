c001b4113f3d4ab87999a2a85eb84d29b613b760
Tomas Milar <tomasmilar4@gmail.com>
2018-06-13 18:31:06 -0300

    meli-client test - initial commit, with basic test passing.
    	* for this to work, the current requirements are:
    		> a local mongod instance running
    		> dotenv .env.test config file with local mondo db URL

---
+++ test/unit/lib/meli-client.test.js
@@ -0,0 +1,41 @@
+import test from 'ava'
+import MeliClient from '../../../lib/meli-client'
+import Account from '../../../model/account'
+
+// Load env variables
+const envTestPath = require('path').resolve(process.cwd(), '.env.test')
+require('dotenv').config({path: envTestPath})
+
+// Mount db connection
+require('../../../config/db')
+
+// Setup meli passport auth strategy //TODO: separate passport auth from router config...
+require('../../../routes/auth')
+
+const DEV_ACCOUNT_USERNAME = 'POKEVENTAS_JUSIMIL'
+let devAccount
+let client
+
+test.before('get dev account for testing', async t => {
+  // Find main dev account
+  devAccount = await Account.findOne({nickname: DEV_ACCOUNT_USERNAME})
+
+  if (!devAccount) {
+    t.fail(`dev account for test (nickname '${DEV_ACCOUNT_USERNAME}') not found in DB`)
+  }
+})
+
+test.serial.before('initialize meli client with dev account', async t => {
+  client = new MeliClient()
+  client.addAccount(devAccount)
+  t.is(client.accounts.length, 1)
+})
+
+test('meli client retrieves sales orders of dev account', async t => {
+  const ordersResponse = await client.getOrders()
+  t.not(ordersResponse, null)
+  t.true(ordersResponse.length > 0)
+  t.is(ordersResponse[0].account.nickname, DEV_ACCOUNT_USERNAME)
+  t.is(ordersResponse[0].error, undefined)
+  t.true(ordersResponse[0].response.results.length > 0)
+})
