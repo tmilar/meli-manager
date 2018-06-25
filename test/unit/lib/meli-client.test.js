1530b33a4b6593fb420e62444707adcb89a0d29d
Tomas Milar <tomasmilar4@gmail.com>
2018-06-25 17:54:01 -0300

    Meli Client test: use env variable DEV_ACCOUNT_USERNAME instead of hardcoding it.

--- test/unit/lib/meli-client.test.js
+++ test/unit/lib/meli-client.test.js
@@ -11,16 +11,16 @@ const Account = require('../../../model/account')
 // Mount db connection
 require('../../../config/db')
 
-const DEV_ACCOUNT_USERNAME = 'POKEVENTAS_JUSIMIL'
+const devAccountUsername = process.env.DEV_ACCOUNT_USERNAME
 let devAccount
 let client
 
 test.before('get dev account for testing', async t => {
   // Find main dev account
-  devAccount = await Account.findOne({nickname: DEV_ACCOUNT_USERNAME})
+  devAccount = await Account.findOne({nickname: devAccountUsername})
 
   if (!devAccount) {
-    t.fail(`dev account for test (nickname '${DEV_ACCOUNT_USERNAME}') not found in DB`)
+    t.fail(`dev account for test (nickname '${devAccountUsername}') not found in DB`)
   }
 })
 
@@ -34,7 +34,7 @@ test('meli client retrieves sales orders of dev account', async t => {
   const ordersResponse = await client.getOrders()
   t.not(ordersResponse, null)
   t.true(ordersResponse.length > 0)
-  t.is(ordersResponse[0].account.nickname, DEV_ACCOUNT_USERNAME)
+  t.is(ordersResponse[0].account.nickname, devAccountUsername)
   t.is(ordersResponse[0].error, undefined)
   t.true(ordersResponse[0].response.results.length > 0)
 })
