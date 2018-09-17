5178f2cac3e6600d4cb29f90b5f049e180e712fc
Tomas Milar <tomasmilar4@gmail.com>
2018-09-17 19:56:18 -0300

    meli-test-account script: now import createMeliTestAccount() recently extracted method.

--- bin/meli-test-account.js
+++ bin/meli-test-account.js
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
 require('dotenv').config({path: require('path').resolve(process.cwd(), '.env.test')})
 
-const req = require('request-promise')
 const db = require('../config/db')
 
-const Account = require('../model/account')
-const {refresh} = require('../config/meli-auth')
 const cliLoginFlow = require('./cli-meli-accounts/cli-login-flow')
+const createMeliTestAccount = require('./cli-meli-accounts/create-meli-test-account')
 
 const devAccountUsername = process.env.DEV_ACCOUNT_USERNAME
 
@@ -30,53 +28,6 @@ function checkNicknameParam(nickname) {
       'Usage: --user=USERNAME or -u=USERNAME . ' +
       'Can also be defined specifying env var DEV_ACCOUNT_USERMAME.')
   }
-}
-
-async function getDevAccount({nickname}) {
-  const account = await Account.findOne({nickname})
-  if (!account) {
-    throw new Error(`Dev Account username '${nickname}' not found in db.`)
-  }
-  if (!account.isAuthorized()) {
-    const accessToken = await refresh.requestNewAccessToken('mercadolibre', account.auth.refreshToken)
-    await account.updateAccessToken(accessToken)
-  }
-  return account
-}
-
-async function requestTestAccount(devAccount) {
-  const testAccountRequestOptions = {
-    method: 'POST',
-    uri: 'https://api.mercadolibre.com/users/test_user',
-    qs: {
-      access_token: devAccount.auth.accessToken
-    },
-    body: {
-      site_id: 'MLA'
-    },
-    json: true // Automatically stringifies the body to JSON
-  }
-  return req(testAccountRequestOptions)
-}
-
-async function createMeliTestAccount(nickname) {
-  if (!nickname || nickname.length === 0) {
-    throw new Error('The owner account nickname is required to request a MeLi Test Account!')
-  }
-  const ownerAccount = await getDevAccount({nickname})
-  console.log(`Requesting test account using dev account '${ownerAccount.nickname}'...`)
-
-  let response
-  try {
-    response = await requestTestAccount(ownerAccount)
-  } catch (error) {
-    const errMsg = `Error requesting test account with dev account '${ownerAccount.nickname}'`
-    if (error) {
-      error.message = `${errMsg}. ${error.message || error}`
-    }
-    throw new Error(error || errMsg)
-  }
-  return response
 }
 
 /**
