108ce56babe1055c8966d0b1da8237a8eb0d4e6f
Tomas Milar <tomasmilar4@gmail.com>
2018-09-21 20:11:01 -0300

    Account model: add 'isNewAccount()' schema method.

--- model/account.js
+++ model/account.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 
+const TIME_NEW_ACCOUNT_MILLIS = 1000 * 60 // 1 minute
+
 const accountSchema = new mongoose.Schema({
   id: Number,
   nickname: String,
@@ -11,6 +13,8 @@ const accountSchema = new mongoose.Schema({
     refreshToken: String,
     expires: Date
   }
+}, {
+  timestamps: true
 })
 
 accountSchema.methods.updateAccessToken = async function (accessToken) {
@@ -25,6 +29,14 @@ accountSchema.methods.updateAccessToken = async function (accessToken) {
 accountSchema.methods.isAuthorized = function () {
   // If we are earlier than expiration date, then it's authorized.
   return new Date() < this.auth.expires
+}
+
+accountSchema.methods.isNewAccount = function () {
+  // An account is new if it was created in the last TIME_NEW_ACCOUNT_MILLIS time.
+  if (!this.createdAt) {
+    return false
+  }
+  return (new Date() - this.createdAt) < TIME_NEW_ACCOUNT_MILLIS
 }
 
 accountSchema.statics.register = async function (profile, auth) {
@@ -48,7 +60,6 @@ accountSchema.statics.register = async function (profile, auth) {
   }
 
   const registered = await this.findOneAndUpdate({id}, account, {upsert: true}).exec()
-  console.log(`Registered ${nickname} mercadolibre account successfully!`)
   return registered
 }
 
