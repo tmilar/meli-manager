eb34a98e3d6fda1917d539d6b5970673b982f006
Tomas Milar <tomasmilar4@gmail.com>
2018-10-06 15:33:56 -0300

    bin/setup script: add optional relative --path [directory] param.

--- bin/setup.js
+++ bin/setup.js
@@ -1,25 +1,37 @@
-/**
- * Initialize .env file
- * @param --force [optional] overrides the .env file even if it exists.
- */
+#!/usr/bin/env node
 'use strict'
 const fs = require('fs')
+const path = require('path')
+const program = require('commander')
 
-const args = process.argv.slice(2)
-const force = args[0] === '--force'
+program
+  .version('0.1.0')
+  .description('Intialize a fresh .env file using an .env.sample file.')
+  .option('-f, --force', 'Override existing file, thus creating a new one.')
+  .option('-p, --path [relativePath]', 'Execute in the specified directory.')
+  .parse(process.argv)
+
+const {force, path: basePath = '.'} = program
 
 const ENV = '.env'
 const ENV_SAMPLE = '.env.sample'
 
 function main() {
-  if (fs.existsSync(ENV) && !force) {
-    console.log(`${ENV} file already exists! Add --force option to override.`)
-    process.exitCode = 1
-    return
+  const envPath = path.join(basePath, ENV)
+  const envSamplePath = path.join(basePath, ENV_SAMPLE)
+
+  console.log(envPath, envSamplePath)
+  if (fs.existsSync(envPath) && !force) {
+    console.log(`${envPath} file already exists! Add --force option to override.`)
+    return process.exit(1)
   }
 
-  fs.createReadStream(ENV_SAMPLE)
-    .pipe(fs.createWriteStream(ENV))
+  if (!fs.existsSync(envSamplePath)) {
+    throw new Error(`${envSamplePath} file doesn't exist!`)
+  }
+
+  fs.createReadStream(envSamplePath)
+    .pipe(fs.createWriteStream(envPath))
 
   console.log(`Created ${ENV} file from ${ENV_SAMPLE}`)
 }
