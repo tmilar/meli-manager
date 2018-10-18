945b6038d35f493a19fc50d2d6cc2ed05adbf4c0
marcospaulucci <marcos287900@gmail.com>
2018-10-18 18:42:21 -0300

    Renamed meli auth config to meliAuth for more clarity

---
+++ routes/meliAuth.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+
+const {passport} = require('../config/meli-auth')
+
+passport.serializeUser((user, cb) => {
+  cb(null, user)
+})
+
+passport.deserializeUser((obj, cb) => {
+  cb(null, obj)
+})
+
+router.get('/mercadolibre', passport.authorize('mercadolibre'))
+
+router.get('/mercadolibre/callback', passport.authorize('mercadolibre', {failureRedirect: '/auth/error'}),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    console.log('Successful login!')
+    res.redirect('/auth/success')
+  })
+
+router.get('/success', (req, res) => res.send('You have successfully logged in'))
+
+router.get('/error', (req, res) => res.send('error logging in'))
+
+module.exports = router
