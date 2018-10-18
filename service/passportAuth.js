833ae946ac48624a75a27a66e463fb0bb5c50e4a
marcospaulucci <marcos287900@gmail.com>
2018-10-18 18:21:33 -0300

    Renamed passport project file to passportAuth to avoid mistaking it for passport package

--- service/passportAuth.js
+++ service/passportAuth.js
@@ -1,35 +1,20 @@
-const passport = require('passport')
+const passportAuth = require('passport')
 const JwtStrategy = require('passport-jwt').Strategy
 const {ExtractJwt} = require('passport-jwt')
 const LocalStrategy = require('passport-local')
-const config = require('../../config')
-const User = require('../models/user')
+const config = require('../config')
+const User = require('../model/user')
 
 // Create local strategy
 const localOptions = {usernameField: 'email'}
 const localLogin = new LocalStrategy(localOptions, async (email, password, done) => {
-  // Verify this email and password, call done with the user
-  // if it is the correct email and password
-  // otherwise, call done with false
   let user
   try {
-    user = await User.findOne({email})
-  } catch (error) {
-    return done(error)
-  }
-  if (!user) {
-    return done(null, false)
-  }
-  // Compare passwords - is `password` equal to user.password?
-  user.comparePassword(password, (err, isMatch) => {
-    if (err) {
-      return done(err)
-    }
-    if (!isMatch) {
-      return done(null, false)
-    }
+    user = await User.signin(email, password)
     return done(null, user)
-  })
+  } catch (error) {
+    return done(error, false)
+  }
 })
 
 // Setup options for JWT Strategy
@@ -43,15 +28,21 @@ const jwtLogin = new JwtStrategy(jwtOptions, async (payload, done) => {
   // See if the user ID in the payload exists in our database
   // If it does, call 'done' with that other
   // otherwise, call done without a user object
+  const userId = payload.sub
   let user
   try {
-    user = await User.findById(payload.sub)
+    user = await User.findById(userId)
   } catch (error) {
     return done(error, false)
   }
   done(null, user || false)
 })
 
-// Tell passport to use this strategy
-passport.use(jwtLogin)
-passport.use(localLogin)
+// Setup method to tell passport to use this strategy
+const setup = () => {
+  passportAuth.use(jwtLogin)
+  passportAuth.use(localLogin)
+}
+
+module.exports = {setup}
+
