a657d7686866b2899221ab74e35f3a6884475360
Tomas Milar <tomasmilar4@gmail.com>
2019-01-06 20:22:45 -0300

    questions service: implement startDate & endDate filters for GET '/question' endpoint.

--- routes/question.js
+++ routes/question.js
@@ -19,9 +19,33 @@ router.post('/answer', async (req, res, next) => {
 
   res.sendStatus(200)
 })
+const _isValidDate = date => Object.prototype.toString.call(date) === '[object Date]'
 
 router.get('/', async (req, res, next) => {
-  const {accounts, store} = req.query
+  const {start, end, accounts, store} = req.query
+
+  // Parse params
+  const dateFormat = 'DD-MM-YY'
+  let startDate
+  let endDate
+
+  if (start) {
+    startDate = moment(start, dateFormat).toDate()
+    if (!_isValidDate(startDate)) {
+      const error = new Error(`Bad request: 'start' date param '${start}' is not properly formatted as '${dateFormat}'`)
+      error.status = 400
+      throw error
+    }
+  }
+
+  if (end) {
+    endDate = moment(end, dateFormat).endOf('day').toDate()
+    if (!_isValidDate(endDate)) {
+      const error = new Error(`Bad request: 'end' date param '${end}' is not properly formatted as '${dateFormat}'`)
+      error.status = 400
+      throw error
+    }
+  }
 
   let accountsArray
   if (accounts) {
@@ -44,7 +68,7 @@ router.get('/', async (req, res, next) => {
     const selectedAccounts = await Account.find(nicknamesFilter)
 
     // 2. fetch remote questions for accounts
-    const meliQuestionFilters = {accounts: selectedAccounts}
+    const meliQuestionFilters = {startDate, endDate, accounts: selectedAccounts}
     questionsResponse = await QuestionService.getQuestions(meliQuestionFilters)
 
     // 3. store selected questions
